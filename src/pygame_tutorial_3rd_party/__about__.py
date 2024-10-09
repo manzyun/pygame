aa787f89e6bb63aa5b7b764f81608c292ff2a15a
manzyun <manzyun@gmail.com>
2024-10-09 13:29:41 +0900

    [WIP][Add] `hatch run basic-02`
    About draw_text.

---
+++ src/pygame_tutorial_3rd_party/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present manzyun <manzyun@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.1.2"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present manzyun <manzyun@gmail.com>
+#
+# SPDX-License-Identifier: MIT
